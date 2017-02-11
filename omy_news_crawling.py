e5fdfe751c7fa1404ac930ed2652b5eb0dd65ded
tempo13 <breakthebias@naver.com>
2017-02-11 15:45:59 +0900

    Add omy_news_crawling.py

--- omy_news_crawling.py
+++ omy_news_crawling.py
@@ -1,17 +1,20 @@
 # -*- coding: CP949 -*-
 import requests
-import codecs
 from bs4 import BeautifulSoup
+
 
 def _spider():
     url = 'http://www.ohmynews.com/NWS_Web/ArticlePage/Total_Article.aspx?PAGE_CD=C0400'
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'lxml')
-    link = soup.select('dt > a')[0]
+    link = soup.select('dt > a')[0]  # beautifulSoup is use array
+    """
+    select daily first news in politcs
+    """
     href = 'http://www.ohmynews.com/' + link.get('href')
-    # print(href)
-    article_crawler(href)
+    article_crawler(href)  # send to text crawler
+
 
 def article_crawler(article_link):
     source_code = requests.get(article_link)
