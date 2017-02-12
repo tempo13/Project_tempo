f9bd0281e615228706b529678ce34b3703bad6ce
tempo13 <breakthebias@naver.com>
2017-02-12 09:51:07 +0900

    Add omy_news_crawling.py

--- omy_news_crawling.py
+++ omy_news_crawling.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 
 
 def _spider():
-    url = 'http://www.ohmynews.com/NWS_Web/ArticlePage/Total_Article.aspx?PAGE_CD=C0400'
+    url = 'http://m.ohmynews.com/'
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'lxml')
-    link = soup.select('dt > a')[0]  # beautifulSoup is use array
+    link = soup.select('li.row_thumb > a.link_thumb')[0]  # beautifulSoup is use array
     """
     select daily first news in politcs
     """
-    href = 'http://www.ohmynews.com/' + link.get('href')
+    href = 'http://m.ohmynews.com' + link.get('href')
     article_crawler(href)  # send to text crawler
 
 
@@ -20,9 +20,10 @@ def article_crawler(article_link):
     source_code = requests.get(article_link)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'lxml')
-    article_text = soup.select('div.article_view > div.at_contents')
+    article_text = soup.select('div.at_contents')
     for content in article_text:
+        print(content.get_text())
         article_content = content.find_all(text=True)
-        print(article_content)
+        #print(article_content)
 
 _spider()
